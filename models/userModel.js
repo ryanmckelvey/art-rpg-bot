d2faea55db05a873cabf0cee2a4b3049711213f3
Ryan McKelvey <ryanmckelvey98@gmail.com>
2025-10-30 00:41:35 +0000

    creating queries

---
+++ models/userModel.js
@@ -0,0 +1,15 @@
+import pool from '../config/db.js';
+
+export const getUserById = async (userId) => {
+  const query = 'SELECT * FROM users WHERE user_id = $1';
+  const values = [userId];
+  const res = await pool.query(query, values);
+  return res.rows;
+}
+
+export const createUser = async (userId, username) => {
+  const query = 'INSERT INTO users (user_id, username) VALUES ($1, $2) RETURNING *';
+  const values = [userId, username];
+  const res = await pool.query(query, values);
+  return res.rows[0];
+}
