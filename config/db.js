e4a286b390aa91ca40748aff7f2e878b23f91e43
Ryan McKelvey <ryanmckelvey98@gmail.com>
2025-10-30 00:41:23 +0000

    adding db connection

---
+++ config/db.js
@@ -0,0 +1,22 @@
+import pkg from 'pg';
+
+const { Pool } = pkg;
+
+const pool = new Pool({
+  user: process.env.USER,
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+  port: process.env.DBPORT,
+});
+
+pool.on('error', (err) => {
+  console.error('Unexpected error on idle client', err);
+  process.exit(-1);
+});
+
+pool.on('connect', () => {
+  console.log('Database connected successfully');
+});
+
+export default pool;
