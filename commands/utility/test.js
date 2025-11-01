dd8905a5be209e4f36a4a236a57e9e95ca27cbb6
Ryan McKelvey <ryanmckelvey98@gmail.com>
2025-11-01 01:01:23 +0000

    making test reply ephemeral

--- commands/utility/test.js
+++ commands/utility/test.js
@@ -2,5 +2,5 @@ import { SlashCommandBuilder } from 'discord.js';
 
 export const data = new SlashCommandBuilder().setName('new-test').setDescription('Test command to check bot functionality');
 export async function execute(interaction) {
-    await interaction.reply('Test command received successfully! ✅');
+    await interaction.reply({ content: 'Test command received successfully! ✅', ephemeral: true });
 }
