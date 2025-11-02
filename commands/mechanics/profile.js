f1b821b6ec22aefc96ebfe22e2813bae41d4ffe6
Ryan McKelvey <ryanmckelvey98@gmail.com>
2025-11-02 22:15:08 +0000

    adding a profile command to display database details for users.

---
+++ commands/mechanics/profile.js
@@ -0,0 +1,23 @@
+import { SlashCommandBuilder } from 'discord.js';
+import { generateProfileMessage } from '../../utils.js';
+
+export const data = new SlashCommandBuilder()
+    .setName('profile')
+    .setDescription('View your RPG profile or that of another player.')
+    .addUserOption(option =>
+        option.setName('player')
+            .setDescription('The player whose profile you want to view')
+            .setRequired(false));
+
+export async function execute(interaction) {
+    let userId;
+    const mentionedUser = interaction.options.getUser('player');
+    if (mentionedUser) {
+        userId = mentionedUser.id;
+    }
+    else {
+        userId = interaction.user.id;
+    }
+    let message = await generateProfileMessage(userId);
+    await interaction.reply({ content: message, ephemeral: mentionedUser ? true : false });
+}
