651633a80adf58805b8719eef9e3a87093094c50
Ryan McKelvey <ryanmckelvey98@gmail.com>
2025-11-02 22:14:41 +0000

    adding a user level add to rpg command

---
+++ commands/moderation/addmetorpg.js
@@ -0,0 +1,28 @@
+import { SlashCommandBuilder } from 'discord.js';
+import { createUser, getUserById } from '../../models/userModel.js';
+import { addMemberToRole } from '../../utils.js';
+
+export const data = new SlashCommandBuilder()
+    .setName('addmetorpg')
+    .setDescription('Get added to the RPG!');
+
+export async function execute(interaction) {
+    let replyMessage;
+    console.log(`Adding player ${interaction.user.username} to the RPG...`);
+    try {
+        if ((await getUserById(interaction.user.id)).length > 0) {
+            replyMessage = { content: `<@${interaction.user.id}> You're already in the RPG! Use /profile to check yourself out!`, ephemeral: true };
+        }
+        else {
+            await createUser(interaction.user.id, interaction.user.username);
+            addMemberToRole(interaction.user.id, interaction.guild, process.env.RPG_PLAYER_ROLE_ID);
+            console.log(`Player ${interaction.user.username} added to the RPG database.`);
+            replyMessage = { content: `Welcome <@${interaction.user.id}> to the Art RPG! You have been granted the RPG Player role!`, ephemeral: false };
+        }
+    }
+    catch (error) {
+        console.error('Error adding player to RPG database:', error);
+        replyMessage = { content: `There was an error adding <@${interaction.user.id}> to the RPG database. Please try again later.`, ephemeral: true };
+    }
+    await interaction.reply(replyMessage);
+}
