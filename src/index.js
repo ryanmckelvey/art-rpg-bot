c75b4f5d58a0b582d50bd3f1408e295c38296a0b
Ryan McKelvey <ryanmckelvey98@gmail.com>
2025-11-05 01:53:22 +0000

    adding reaction listener client

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import { join } from 'node:path';
 import { readdirSync } from 'node:fs';
 import path from 'path'
 import { fileURLToPath, pathToFileURL } from 'node:url';
-import { Client, Collection, Events, GatewayIntentBits, MessageFlags } from 'discord.js';
+import { Client, Collection, Events, GatewayIntentBits, Partials } from 'discord.js';
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions],
+    partials: [Partials.Message, Partials.Channel, Partials.Reaction]
+});
 
 client.login(process.env.DISCORD_TOKEN);
 
@@ -20,11 +23,11 @@ const __dirname = path.dirname(__filename);
 const foldersPath = join(__dirname, '../commands');
 const commandFolders = readdirSync(foldersPath);
 for (const folder of commandFolders) {
-	const commandsPath = path.join(foldersPath, folder);
-	const commandFiles = readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
-	for (const file of commandFiles) {
-		const filePath = path.join(commandsPath, file);
-		try {
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        try {
             var fileUrl = pathToFileURL(filePath).href;
             var imported = await import(fileUrl);
             var command = imported.default ?? imported;
@@ -38,7 +41,7 @@ for (const folder of commandFolders) {
         catch (error) {
             console.error(`Error importing command at ${filePath}:`, error);
         }
-	}
+    }
 }
 
 client.on(Events.InteractionCreate, async interaction => {
@@ -55,10 +58,24 @@ client.on(Events.InteractionCreate, async interaction => {
         await command.execute(interaction);
     } catch (error) {
         console.error(`Error executing ${interaction.commandName}\n`, error);
-        if(interaction.replied || interaction.deferred) {
+        if (interaction.replied || interaction.deferred) {
             await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
         } else {
             await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
         }
+    }
+});
+
+client.on(Events.MessageReactionAdd, async (reaction, user) => {
+    if (reaction.partial) {
+        try {
+            await reaction.fetch();
+        } catch (error) {
+            console.error('Something went wrong when fetching the message reaction: ', error);
+            return;
+        }
+    }
+    if (reaction.message.channel.id === process.env.ART_REVIEW_CHANNEL_ID) {
+        console.log(`Reaction added by ${user.username} to message ID: ${reaction.message.id}`);
     }
 });
