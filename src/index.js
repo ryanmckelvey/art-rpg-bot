621e6233d8ee6bec295109194f07addfefd51c2b
Ryan McKelvey <ryanmckelvey98@gmail.com>
2025-11-08 17:08:31 +0000

    Added logic to handle approvals and disapprovals

--- src/index.js
+++ src/index.js
@@ -45,24 +45,35 @@ for (const folder of commandFolders) {
 }
 
 client.on(Events.InteractionCreate, async interaction => {
-    if (!interaction.isCommand()) return;
-
-    const command = interaction.client.commands.get(interaction.commandName);
-
-    if (!command) {
-        console.error(`No command matching ${interaction.commandName} was found.`);
+    if (!interaction.isCommand() && interaction.type !== 3) {
+        console.log("Error, unknown interaction", interaction);
         return;
-    }
+    };
 
-    try {
-        await command.execute(interaction);
-    } catch (error) {
-        console.error(`Error executing ${interaction.commandName}\n`, error);
-        if (interaction.replied || interaction.deferred) {
-            await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-        } else {
-            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+    if (interaction.isCommand()) {
+        const command = interaction.client.commands.get(interaction.commandName);
+
+        if (!command) {
+            console.error(`No command matching ${interaction.commandName} was found.`);
+            return;
         }
+
+        try {
+            await command.execute(interaction);
+        } catch (error) {
+            console.error(`Error executing ${interaction.commandName}\n`, error);
+            if (interaction.replied || interaction.deferred) {
+                await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+            } else {
+                await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+            }
+        }
+    }
+    else if (interaction.type === 3) {
+        const imageUrl = interaction.message.components[1].items[0].media.data.url;
+        const approvalStatus = interaction.customId.includes("approve") ? "Approved" : "Rejected";
+        console.log("Button clicked on Message: ", imageUrl);
+        await interaction.reply({content: `This has been: ${approvalStatus}`});
     }
 });
 
