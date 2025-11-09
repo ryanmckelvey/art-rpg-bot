488ca20a1ed80d495066ce54059e04012f6c2a87
Ryan McKelvey <ryanmckelvey98@gmail.com>
2025-11-09 23:15:56 +0000

    adding money check to egg command

--- commands/mechanics/eggme.js
+++ commands/mechanics/eggme.js
@@ -1,9 +1,11 @@
 import { SlashCommandBuilder } from 'discord.js';
 import { generateEggMessage } from '../../src/utils/mechanics/egg/pokeGenerator.js';
+import { transactionForUser } from '../../src/utils/economy/transaction.js';
 
 export const data = new SlashCommandBuilder().setName('eggme').setDescription('Receive a random egg');
 export async function execute(interaction) {
     let userId = interaction.user.id;
+    if(await transactionForUser(userId, -500, interaction)) return;
     let message = generateEggMessage(userId);
     await interaction.reply(message);
 }
