10906eca1d4da6f5de1c7886f7494c50a4ee4cf8
Ryan McKelvey <ryanmckelvey98@gmail.com>
2025-11-09 20:39:01 +0000

    replacing more ephemeral: true flags

--- src/index.js
+++ src/index.js
@@ -64,9 +64,9 @@ client.on(Events.InteractionCreate, async interaction => {
         } catch (error) {
             console.error(`Error executing ${interaction.commandName}\n`, error);
             if (interaction.replied || interaction.deferred) {
-                await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+                await interaction.followUp({ content: 'There was an error while executing this command!', flags: "Ephemeral" });
             } else {
-                await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+                await interaction.reply({ content: 'There was an error while executing this command!', flags: "Ephemeral" });
             }
         }
     }
