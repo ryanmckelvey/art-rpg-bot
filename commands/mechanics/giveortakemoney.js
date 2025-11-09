a30aa2f0b6ec54b46a3d001bd5c92c068628521f
Ryan McKelvey <ryanmckelvey98@gmail.com>
2025-11-09 22:06:05 +0000

    new slash command to add or take away money for a user

---
+++ commands/mechanics/giveortakemoney.js
@@ -0,0 +1,20 @@
+import { SlashCommandBuilder } from "discord.js";
+import { updateUserMoney } from "../../src/models/userModel.js";
+
+export const data = new SlashCommandBuilder()
+    .setName('giveortakemoney')
+    .setDescription('Give or take money depending on the value input')
+    .addUserOption(option =>
+        option.setName('player')
+            .setDescription('Player whos Poké will be adjusted')
+            .setRequired(true))
+    .addNumberOption(option =>
+        option.setName('amount')
+            .setDescription('Amount money will be adjusted by')
+            .setRequired(true));
+
+export async function execute(interaction) {
+    updateUserMoney(interaction.options.getNumber('amount'), interaction.options.getUser('player').id);
+    console.log(interaction.options.getNumber('amount'));
+    await interaction.reply({ content: "This will be a command to adjust money of a target player", flags: "Ephemeral" });
+}
