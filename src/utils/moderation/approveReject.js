156c43da128c36097b7ef2d9014a1f80c7a5164c
Ryan McKelvey <ryanmckelvey98@gmail.com>
2025-11-12 01:09:49 +0000

    early stages of a more in depth approval process for art

--- src/utils/moderation/approveReject.js
+++ src/utils/moderation/approveReject.js
@@ -1,14 +1,23 @@
 import { MessagePayload } from "discord.js";
+import { approvalProcessModal } from '../../const/modals.js';
 
 export async function approveArtwork(interaction) {
     const message = interaction.message;
     const imageUrl = interaction.message.components[1].items[0].media.data.url;
     const newComponents = [...message.components];
-    newComponents.pop();
     const userId = message.components[0].data.content.split(/[@>]/)[1];
     const approvalStatus = interaction.customId.includes("approve") ? "Approved" : "Rejected";
-    newComponents[0].data.content = `Artwork submitted by <@${userId}>: ${imageUrl} | Has been ${approvalStatus} by <@${interaction.user.id}>`
     const payload = MessagePayload.create(message).resolveBody();
+
+    if (approvalStatus === "Approved") {
+        newComponents.pop();
+        newComponents[0].data.content = `Artwork submitted by <@${userId}>: ${imageUrl} | Is being reviewed by <@${interaction.user.id}>`
+        payload.body.components = newComponents;
+        message.edit(payload);
+        await interaction.showModal(approvalProcessModal());
+    }
+
+    newComponents[0].data.content = `Artwork submitted by <@${userId}>: ${imageUrl} | Has been ${approvalStatus} by <@${interaction.user.id}>`
     payload.body.components = newComponents;
     message.edit(payload);
 }
